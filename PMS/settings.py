616d2dda157aa501747db96edb7f7155a72ae252
resulcaliskan <resulcaliskan@yahoo.com>
2021-04-26 14:20:28 +0300

    autodefaulkey line added to settings.py

--- PMS/settings.py
+++ PMS/settings.py
@@ -105,12 +105,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
-
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 LOGIN_URL = '/user/login/'
@@ -120,3 +119,5 @@ LOGOUT_REDIRECT_URL = '/user/login/'
 # MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_ROOT = BASE_DIR
 MEDIA_URL = '/media/'
+
+DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'
